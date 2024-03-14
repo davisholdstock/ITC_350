3bd046f14c54c706a27db28610efcc15147215b2
Matthew Day <matthewjday1999@gmail.com>
2024-03-14 14:53:26 -0600

    Finished CREATE VIEW Statements

--- Code/views.sql
+++ Code/views.sql
@@ -7,9 +7,10 @@ INNER JOIN Recipe ON Cookbook.CBRecipeID = Recipe.RecipeID;
 
 -- VIEW Statement for individual recipes
 CREATE VIEW IndividualRecipe AS
-SELECT RecipeID, Rating, Difficulty, Directions, Duration, Title, IngredientID, Count, Units
+SELECT RecipeID, Rating, Difficulty, Directions, Category, Picture, Duration, Title, IngredientID, Count, Units, ItemName
 FROM Recipe
-INNER JOIN Ingredient ON Recipe.RecipeID = Ingredient.RecipeID;
+INNER JOIN Ingredient ON Recipe.RecipeID = Ingredient.RecipeID
+INNER JOIN ITEM ON Ingredient.IngredientID = Item.ItemID
 
 -- VIEW Statement for User Shopping Lists
 CREATE VIEW myShoppingList AS
@@ -20,15 +21,19 @@ INNER JOIN Item ON ShoppingList.SLItemID = Item.ItemID;
 
 -- VIEW Statement for User Account information
 CREATE VIEW AccountPage AS
-SELECT UserID, Username, Password
-FROM User
+SELECT UserID, Username, Password, PUserID, FUserID
+FROM Friendship
+INNER JOIN User ON User.UserID = Friendship.PUserID
 
 -- VIEW Statement for Recipe Search page, Do we need this one?
 CREATE VIEW FindRecipe AS
-
+SELECT ItemID, ItemName, IngredientID, RecipeID, Category
+FROM Ingredient
+INNER JOIN Item ON Item.ItemID = Ingredient.IngredientID
+INNER JOIN Recipe ON Ingredient.IRecipeID = Recipe.RecipeID;
 
 -- VIEW STATEMENT for All Recipes
 CREATE VIEW AllRecipes AS
-SELECT RecipeID, Rating, Difficulty, Directions, Duration, Title, IngredientID, Count, Units
+SELECT RecipeID, Rating, Difficulty, Category, Duration, Picture, Title, IngredientID, ItemName, ItemID
 FROM Recipe
 INNER JOIN Ingredient ON Recipe.RecipeID = Ingredient.RecipeID;
