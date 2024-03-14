6abfd1cf8b6aa7b2d58b52d15a2384350b6f0703
Matthew Day <matthewjday1999@gmail.com>
2024-03-14 14:08:47 -0600

    Co-authored-by: brromney <brromney@users.noreply.github.com> Co-authored-by: tdespain10 <tdespain10@users.noreply.github.com>

--- Code/views.sql
+++ Code/views.sql
@@ -1,7 +1,34 @@
+-- VIEW Statement for User Cookbook
 CREATE VIEW myCookbook AS 
 SELECT UserID, Username, RecipeID, Title, Rating, Difficulty, Duration 
 FROM Cookbook 
 INNER JOIN User ON User.UserID = Cookbook.CBUserID 
 INNER JOIN Recipe ON Cookbook.CBRecipeID = Recipe.RecipeID;
 
--- select * from myCookbook;
+-- VIEW Statement for individual recipes
+CREATE VIEW IndividualRecipe AS
+SELECT RecipeID, Rating, Difficulty, Directions, Duration, Title, IngredientID, Count, Units
+FROM Recipe
+INNER JOIN Ingredient ON Recipe.RecipeID = Ingredient.RecipeID;
+
+-- VIEW Statement for User Shopping Lists
+CREATE VIEW myShoppingList AS
+SELECT UserID, Username, SLUserID, SLItemID, ItemID, ItemName
+FROM Item
+INNER JOIN User ON User.UserID = ShoppingList.SLUserID
+INNER JOIN Item ON ShoppingList.SLItemID = Item.ItemID;
+
+-- VIEW Statement for User Account information
+CREATE VIEW AccountPage AS
+SELECT UserID, Username, Password
+FROM User
+
+-- VIEW Statement for Recipe Search page, Do we need this one?
+CREATE VIEW FindRecipe AS
+
+
+-- VIEW STATEMENT for All Recipes
+CREATE VIEW AllRecipes AS
+SELECT RecipeID, Rating, Difficulty, Directions, Duration, Title, IngredientID, Count, Units
+FROM Recipe
+INNER JOIN Ingredient ON Recipe.RecipeID = Ingredient.RecipeID;
