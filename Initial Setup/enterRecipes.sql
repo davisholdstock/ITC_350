28deb26e4899107d553098d5c3ca417a6d62f9ea
Davis Holdstock <daviswholdstock@gmail.com>
2024-03-21 13:35:24 -0600

    Updated(ourRecipes.csv, enterRecipes.sql) added the first recipe to test if it works

--- Initial Setup/enterRecipes.sql
+++ Initial Setup/enterRecipes.sql
@@ -1,6 +1,6 @@
-INSERT INTO Recipe (Rating, Difficulty, Directions, Duration, Title)
+INSERT INTO Recipe (Rating, Difficulty, Directions, Duration, Title, Category, Picture)
 VALUES
-(),
+(5, 1, "For the French toast: Grease the baking pan with butter. Tear the bread into chunks (or cut into cubes) and evenly distribute in the pan. Crack the eggs in a big bowl. Whisk together the eggs; milk; cream; granulated sugar; brown sugar and vanilla. Pour evenly over the bread. Cover the pan tightly and store in the fridge until needed (overnight preferably). For the topping: Mix the flour; brown sugar; cinnamon; salt and some nutmeg in a separate bowl. Stir together using a fork. Add the butter and with a pastry cutter and mix it all together until the mixture resembles fine pebbles. Store in a re-sealable plastic bag in the fridge. When you're ready to bake the casserole preheat the oven to 350 degrees F. Remove the casserole from the fridge and sprinkle the topping over the top. Bake for 45 minutes for a softer more bread pudding texture or for 1 hour or more for a firmer crisper texture. Scoop out individual portions. Top with butter and drizzle with warm pancake syrup and sprinkle with blueberries.", 60, 'Cinnamon Baked French Toast', 'Breakfast', 'cinnamon_baked_french_toast.jpeg'),
 (),
 (),
 (),
