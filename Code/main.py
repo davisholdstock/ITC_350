7d471c5a3fc7e4a88ea3b21b93856754228a7a6b
Matthew Day <108026517+matthewjday@users.noreply.github.com>
2024-04-04 12:23:56 -0600

    Update main.py to allow login from anywhere

--- Code/main.py
+++ Code/main.py
@@ -86,4 +86,4 @@ def add_item():
 
 # listen on port 8080
 if __name__ == "__main__":
-    app.run(port=8080, debug=True) # TODO: Students PLEASE remove debug=True when you deploy this for production!!!!!
+    app.run(host="0.0.0.0", port=8080, debug=True) # TODO: Students PLEASE remove debug=True when you deploy this for production!!!!!
