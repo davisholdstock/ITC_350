cd0c714df962472c5a5e0e42383b71a3d7a744bc
Matthew Day <matthewjday1999@gmail.com>
2024-04-04 12:47:06 -0600

    edited the createWebUser file

--- Initial Setup/createWebUser.sql
+++ Initial Setup/createWebUser.sql
@@ -1,3 +1,3 @@
-CREATE USER '<insert_name>'@'172.16.32.7' IDENTIFIED BY '<your-password>'
+CREATE USER 'webuser'@'172.16.32.7' IDENTIFIED BY '<your-password>'
 
 GRANT SELECT INSERT UPDATE DELETE ON cupboard_cooking.* TO '<insert_name>'@'172.16.32.7'
