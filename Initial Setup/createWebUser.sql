579b9fb04e25c374f7eed2824db8df27b275befd
tdespain10 <tdespain10@gmail.com>
2024-04-17 12:38:27 -0600

    changed filepath on inserts

--- Initial Setup/createWebUser.sql
+++ Initial Setup/createWebUser.sql
@@ -1,3 +1,3 @@
 CREATE USER 'webuser'@'172.16.32.7' IDENTIFIED BY '<your-password>'
 
-GRANT SELECT INSERT UPDATE DELETE ON cupboard_cooking.* TO 'webuser'@'172.16.32.7'
+GRANT SELECT,INSERT,UPDATE,DELETE ON cupboard_cooking.* TO 'webuser'@'172.16.32.7'
